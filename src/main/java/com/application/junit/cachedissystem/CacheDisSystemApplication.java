3f08dffe86c7ca8b45a163d20f79680513bc4e9e
parth karma <codeparthk@gmail.com>
2025-06-10 10:53:44 +0530

    inserted dependencies

---
+++ src/main/java/com/application/junit/cachedissystem/CacheDisSystemApplication.java
@@ -0,0 +1,13 @@
+package com.application.junit.cachedissystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDisSystemApplication {
+
+ public static void main(String[] args) {
+  SpringApplication.run(CacheDisSystemApplication.class, args);
+ }
+
+}
---
+++ src/test/java/com/application/junit/cachedissystem/CacheDisSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.junit.cachedissystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheDisSystemApplicationTests {
+
+ @Test
+ void contextLoads() {
+ }
+
+}
